a74f84c9df5a52f065f3234ada49ec3a621bbc47
son.tran <son.tran@team.enouvo.com>
2023-03-29 14:27:32 +0700

    feat(master): optimize img

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface Content {
+  title: string;
+  description: string;
+  pubDate: string;
+  imgSrc: string;
+  imgAlt: string;
+  draft: boolean;
+}
--- src/utils/Posts.ts
+++ src/utils/Posts.ts
@@ -1,6 +1,6 @@
 import type { MarkdownInstance } from 'astro';
 
-import type { Content } from '@/partials/BlogPost';
+import type { Content } from '../types/index';
 
 export const sortByDate = (posts: MarkdownInstance<Content>[]) => {
   return posts.sort((a, b) => new Date(b.frontmatter.pubDate).valueOf() - new Date(a.frontmatter.pubDate).valueOf());
