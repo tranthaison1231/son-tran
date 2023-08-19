267b4188710081dafd81f02839b354652d85a692
son.tran <son.tran@team.enouvo.com>
2023-08-19 16:47:31 +0700

    feat(master): fix image

--- src/partials/About/ServiceCard.tsx
+++ src/partials/About/ServiceCard.tsx
@@ -8,7 +8,7 @@ const ServiceCard = ({ title, icon }: Props) => {
     <div>
       <div className="w-full animate-fade-right rounded-[20px] p-[1px] shadow-sm">
         <div className="flex min-h-[280px] flex-col items-center justify-evenly rounded-[20px] px-12 py-5">
-          <img src={icon} alt="web-development" className="h-16 w-16 object-contain" />
+          <img src={icon} alt="web-development" width={64} height={64} className="object-contain" />
           <h3 className="text-center text-[20px] font-bold text-white">{title}</h3>
         </div>
       </div>
