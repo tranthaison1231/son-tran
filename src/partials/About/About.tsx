ecdee46ba31b3d9b931824f185de5709453051a5
son.tran <son.tran@team.enouvo.com>
2023-08-20 11:11:23 +0700

    feat(master): update blog

--- src/partials/About/About.tsx
+++ src/partials/About/About.tsx
@@ -1,32 +1,25 @@
-import { useInView } from 'framer-motion';
-import { useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import { AppConfig, Services } from '@/utils/app-config';
 
 import ServiceCard from './ServiceCard';
 
 const About = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
-
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
   return (
     <div ref={ref} className="relative z-0 mx-auto max-w-7xl">
-      {isInView && (
-        <>
-          <div className="animate-fade-down">
-            <p className="text-[14px] uppercase tracking-wider sm:text-[18px]">Introduction</p>
-            <h2 className="xs:text-[40px] text-[30px] font-black text-white sm:text-[50px] md:text-[60px]">
-              Overview.
-            </h2>
-            <p className="mt-4 max-w-3xl text-[17px] leading-[30px]">{AppConfig.bio}</p>
-          </div>
-          <div className="mt-20 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4">
-            {Services.map((service) => (
-              <ServiceCard key={service.title} {...service} />
-            ))}
-          </div>
-        </>
-      )}
+      <div className={inView ? 'animate-fade-down' : ''}>
+        <p className="text-[14px] uppercase tracking-wider sm:text-[18px]">Introduction</p>
+        <h2 className="xs:text-[40px] text-[30px] font-black text-white sm:text-[50px] md:text-[60px]">Overview.</h2>
+        <p className="mt-4 max-w-3xl text-[17px] leading-[30px]">{AppConfig.bio}</p>
+      </div>
+      <div className="mt-20 grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4">
+        {Services.map((service) => (
+          <ServiceCard key={service.title} {...service} />
+        ))}
+      </div>
     </div>
   );
 };
