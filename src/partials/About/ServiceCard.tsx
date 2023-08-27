18bef66e477acc88a54af674ff2c376b63541521
son.tran <son.tran@team.enouvo.com>
2023-08-27 15:47:59 +0700

    feat(master): update package

--- src/partials/About/ServiceCard.tsx
+++ src/partials/About/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard = ({ title, icon }: Props) => {
   return (
     <div>
       <div className="w-full rounded-3xl p-1 shadow-md">
-        <div className="flex min-h-[280px] flex-col items-center justify-evenly rounded-3xl px-12 py-5">
+        <div className="min-h-72 flex flex-col items-center justify-evenly rounded-3xl px-12 py-5">
           <img src={icon.src} alt="web-development" width={64} height={64} className="object-contain" loading="lazy" />
           <h3 className="text-center text-xl font-bold text-foreground">{title}</h3>
         </div>
