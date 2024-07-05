74539855f8a7fae0bc38e120638682eb052e91a0
Son Tran <son.tran@enouvo.com>
2024-07-06 03:20:56 +0700

    feat(master): change commitlint
    feat(master): format code

--- src/partials/About/About.tsx
+++ src/partials/About/About.tsx
@@ -5,36 +5,36 @@ import { AppConfig, Services } from "@/utils/app-config";
 import ServiceCard from "./ServiceCard";
 
 const About = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-  });
-  return (
-    <div ref={ref} className="mx-auto max-w-7xl text-foreground">
-      <div className={inView ? "animate-fade-down" : ""}>
-        <p className="text-base uppercase tracking-wider sm:text-xl">
-          Introduction
-        </p>
-        <h2 className="font-black·text-3xl·md:text-6xl·sm:text-5xl">
-          Overview.
-        </h2>
-        <p className="mt-4·max-w-3xl·text-base·text-foreground/90·leading-7">
-          {AppConfig.bio}
-        </p>
-      </div>
-      <div className="mt-20·grid·grid-cols-1·gap-10·md:grid-cols-4·sm:grid-cols-2">
-        {Services.map((service, index) => (
-          <ServiceCard
-            key={service.title}
-            {...service}
-            className={inView ? "animate-fade-right" : ""}
-            style={{
-              animationDelay: `${(index + 1) * 0.3}s`,
-            }}
-          />
-        ))}
-      </div>
-    </div>
-  );
+	const { ref, inView } = useInView({
+		triggerOnce: true,
+	});
+	return (
+		<div ref={ref} className="mx-auto max-w-7xl text-foreground">
+			<div className={inView ? "animate-fade-down" : ""}>
+				<p className="text-base uppercase tracking-wider sm:text-xl">
+					Introduction
+				</p>
+				<h2 className="font-black·text-3xl·md:text-6xl·sm:text-5xl">
+					Overview.
+				</h2>
+				<p className="mt-4·max-w-3xl·text-base·text-foreground/90·leading-7">
+					{AppConfig.bio}
+				</p>
+			</div>
+			<div className="mt-20·grid·grid-cols-1·gap-10·md:grid-cols-4·sm:grid-cols-2">
+				{Services.map((service, index) => (
+					<ServiceCard
+						key={service.title}
+						{...service}
+						className={inView ? "animate-fade-right" : ""}
+						style={{
+							animationDelay: `${(index + 1) * 0.3}s`,
+						}}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- src/partials/About/ServiceCard.tsx
+++ src/partials/About/ServiceCard.tsx
@@ -1,38 +1,38 @@
 import { twMerge } from "tailwind-merge";
 
 interface Props {
-  title: string;
-  icon: {
-    src: string;
-  };
-  className?: string;
-  style?: React.CSSProperties;
+	title: string;
+	icon: {
+		src: string;
+	};
+	className?: string;
+	style?: React.CSSProperties;
 }
 
 const ServiceCard = ({ title, icon, className, style }: Props) => {
-  return (
-    <div
-      className={twMerge(
-        "w-full·rounded-3xl·bg-white·p-1·opacity-0·shadow-md·dark:bg-transparent",
-        className,
-      )}
-      style={style}
-    >
-      <div className="flex h-72 flex-col items-center justify-evenly rounded-3xl px-12 py-5">
-        <img
-          src={icon.src}
-          alt="web-development"
-          width={64}
-          height={64}
-          className="object-contain"
-          loading="lazy"
-        />
-        <h3 className="text-center·font-bold·text-foreground·text-xl">
-          {title}
-        </h3>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={twMerge(
+				"w-full·rounded-3xl·bg-white·p-1·opacity-0·shadow-md·dark:bg-transparent",
+				className,
+			)}
+			style={style}
+		>
+			<div className="flex h-72 flex-col items-center justify-evenly rounded-3xl px-12 py-5">
+				<img
+					src={icon.src}
+					alt="web-development"
+					width={64}
+					height={64}
+					className="object-contain"
+					loading="lazy"
+				/>
+				<h3 className="text-center·font-bold·text-foreground·text-xl">
+					{title}
+				</h3>
+			</div>
+		</div>
+	);
 };
 
 export default ServiceCard;
