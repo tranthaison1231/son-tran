a5860ff93427ecbd1327d61037806edb520853c8
Son Tran <son.tran@enouvo.com>
2024-07-06 03:28:04 +0700

    feat(master): format code

--- src/partials/About/ServiceCard.tsx
+++ src/partials/About/ServiceCard.tsx
@@ -1,38 +1,41 @@
 import { twMerge } from "tailwind-merge";
+import { BackgroundGradient } from "../ui/BackgroundGradient";
 
 interface Props {
-	title: string;
-	icon: {
-		src: string;
-	};
-	className?: string;
-	style?: React.CSSProperties;
+  title: string;
+  icon: {
+    src: string;
+  };
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 const ServiceCard = ({ title, icon, className, style }: Props) => {
-	return (
-		<div
-			className={twMerge(
-				"w-full·rounded-3xl·bg-white·p-1·opacity-0·shadow-md·dark:bg-transparent",
-				className,
-			)}
-			style={style}
-		>
-			<div className="flex h-72 flex-col items-center justify-evenly rounded-3xl px-12 py-5">
-				<img
-					src={icon.src}
-					alt="web-development"
-					width={64}
-					height={64}
-					className="object-contain"
-					loading="lazy"
-				/>
-				<h3 className="text-center·font-bold·text-foreground·text-xl">
-					{title}
-				</h3>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={twMerge(
+        "w-full rounded-3xl p-1 opacity-0 shadow-md ",
+        className,
+      )}
+      style={style}
+    >
+      <BackgroundGradient>
+        <div className="flex h-72 flex-col items-center justify-evenly rounded-3xl px-12 py-5">
+          <img
+            src={icon.src}
+            alt="web-development"
+            width={64}
+            height={64}
+            className="object-contain"
+            loading="lazy"
+          />
+          <h3 className="text-center font-medium text-foreground text-white text-xl ">
+            {title}
+          </h3>
+        </div>
+      </BackgroundGradient>
+    </div>
+  );
 };
 
 export default ServiceCard;
